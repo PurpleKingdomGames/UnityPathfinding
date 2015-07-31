41d10b8b2980cec2cd5482d45a8188282a41eac3
David North <bigchimp2005+github@gmail.com>
2015-07-31 18:00:49 +0100

    Adds skeleton for Grid building component

---
+++ Assets/PurpleKingdomGames/Pathfinding/Components/GridBuilderComponent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace PurpleKingdomGames.Unity.Components
+{
+    public class GridBuilderComponent : MonoBehaviour
+    {
+        // Use this for initialization
+        private void Start()
+        {
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            // TODO: Show a grid in the editor window (only when game isn't running)
+            // TODO: Use grid location to build grid once, when starting
+            // TODO: Allow the grid to be saved to a file
+            // TODO: Allow the grid to be loaded from a file
+            // TODO: Create seeker component
+            // TODO: Allow the Seeker to seek Synchronously
+            // TODO: Allow the Seeker to seek Asynchronously
+            // TODO: Translate Point2D grid positions to world space
+        }
+    }
+}
