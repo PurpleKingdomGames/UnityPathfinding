7373a89d32318f8341d2f6a97dd7d83de5bed03f
David North <bigchimp2005+github@gmail.com>
2015-07-31 18:00:25 +0100

    Adds basic grid builder functionality

---
+++ Assets/PurpleKingdomGames/Pathfinding/Core/GridBuilder2D.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using PurpleKingdomGames.Core;
+using PurpleKingdomGames.Core.Pathfinding;
+
+namespace PurpleKingdomGames.Unity.Core {
+    public static class GridBuilder2D
+    {
+        public static GridNode2D[,] BuildGrid(Rect bounds, float nodeSize)
+        {
+            return BuildGrid(bounds, nodeSize, Physics2D.AllLayers, new Dictionary<string, int>());
+        }
+
+        public static GridNode2D[,] BuildGrid(Rect bounds, float nodeSize, Dictionary<string, int> tagPenalties)
+        {
+            return BuildGrid(bounds, nodeSize, Physics2D.AllLayers, tagPenalties);
+        }
+
+        public static GridNode2D[,] BuildGrid(Rect bounds, float nodeSize, LayerMask layerMask)
+        {
+            return BuildGrid(bounds, nodeSize, layerMask, new Dictionary<string, int>());
+        }
+
+        public static GridNode2D[,] BuildGrid(Rect bounds, float nodeSize, LayerMask layerMask, Dictionary<string, int> tagPenalties)
+        {
+            float xDistance = bounds.xMax - bounds.xMin;
+            float yDistance = bounds.yMax - bounds.yMin;
+            int gridX = (int) Mathf.Ceil(xDistance / nodeSize);
+            int gridY = (int) Mathf.Ceil(yDistance / nodeSize);
+
+            GridNode2D[,] returnVal = new GridNode2D[gridX, gridY];
+            int nodeY = 0;
+
+            for (float y = bounds.yMin; y < bounds.yMax; y += nodeSize) {
+                // trace ray from left to right
+                float distanceSet = 0;
+                int nodeX = 0;
+                while (distanceSet <= xDistance) {
+                    float distanceLeft = xDistance - distanceSet;
+                    Vector2 origin = new Vector2(bounds.xMin + distanceSet, y);
+                    RaycastHit2D rayCast = Physics2D.Raycast(origin, Vector2.right, distanceLeft, layerMask);
+
+                    // If it hits something then mark each entry up to that point as passable
+                    float hitPoint = Mathf.Floor(rayCast.distance / nodeSize) * nodeSize;
+                    if (rayCast.collider == null) {
+                        hitPoint = distanceLeft;
+                    }
+
+                    Debug.DrawRay(origin, Vector2.right * hitPoint);
+
+                    for (float i = 0; i < hitPoint; i += nodeSize) {
+                        returnVal[nodeX, nodeY] = new GridNode2D() {
+                            Passable = true
+                        };
+                        nodeX++;
+                    }
+
+                    // Mark the point it hit as impassable
+                    distanceSet += (hitPoint + nodeSize);
+                    if (distanceSet <= xDistance && rayCast.collider == null) {
+                        nodeX++;
+                        bool passable = false;
+                        int penalty = 0;
+
+                        if (tagPenalties.ContainsKey(rayCast.collider.tag)) {
+                            penalty = tagPenalties[rayCast.collider.tag];
+                            passable = true;
+                        }
+
+                        returnVal[nodeX, nodeY] = new GridNode2D() {
+                            Passable = passable,
+                            Penalty = penalty
+                        };
+                    }
+                    // Repeat until there is no distance to go
+                }
+
+                nodeY++;
+            }
+
+            return returnVal;
+        }
+    }
+}
