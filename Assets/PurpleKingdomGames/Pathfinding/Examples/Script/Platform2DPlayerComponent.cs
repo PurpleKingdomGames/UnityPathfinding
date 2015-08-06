40403a20f0ab96fa89549fa3c5bcedfe44396cac
David North <bigchimp2005+github@gmail.com>
2015-08-06 13:47:11 +0100

    Adds basic player control

---
+++ Assets/PurpleKingdomGames/Pathfinding/Examples/Script/Platform2DPlayerComponent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using PurpleKingdomGames.Unity.Components;
+
+public class Platform2DPlayerComponent : MonoBehaviour {
+
+	private AStarSeeker2DComponent seeker;
+	private Vector2[] path;
+
+	// Use this for initialization
+	private void Update () {
+		if (seeker == null) {
+			seeker = GetComponent<AStarSeeker2DComponent> ();
+		}
+
+		if (path == null && seeker.IsReady) {
+			path = seeker.Seek (new Vector2 (7.9f, -5.7f));
+		}
+	}
+}
