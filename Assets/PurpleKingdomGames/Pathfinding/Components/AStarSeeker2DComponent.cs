b6bcfe3e8be887233d2c32e67d046cef0a6258d4
David North <bigchimp2005+github@gmail.com>
2015-08-06 13:46:27 +0100

    Adds basic grid building and seeking

--- Assets/PurpleKingdomGames/Pathfinding/Components/AStarSeeker2DComponent.cs
+++ Assets/PurpleKingdomGames/Pathfinding/Components/AStarSeeker2DComponent.cs
@@ -1,17 +1,103 @@
 ﻿using UnityEngine;
+using System.Threading;
+using PurpleKingdomGames.Core;
+using PurpleKingdomGames.Core.Pathfinding;
+using PurpleKingdomGames.Core.Pathfinding.Seekers;
 
 namespace PurpleKingdomGames.Unity.Components
 {
 	public class AStarSeeker2DComponent : MonoBehaviour {
-
-		// Use this for initialization
-		void Start () {
-		
+		public enum StartPositionType
+		{
+			Top, Middle, Bottom
 		}
-		
-		// Update is called once per frame
-		void Update () {
-		
+
+		public bool IsReady {
+			get { return GridBuilder.Grid != null; }
+		}
+
+		public StartPositionType StartPathFrom = StartPositionType.Middle;
+		public GridBuilder2DComponent GridBuilder;
+
+		public Vector2[] Seek(Vector2 target)
+		{
+			return Seek (GetStartPosition(), target);
+		}
+
+		public Vector2[] Seek(Vector2 start, Vector2 target)
+		{
+			if (!IsReady) {
+				throw new UnassignedReferenceException("Grid builder component has not initialised");
+			}
+
+			// Get the offset for the grid
+			float offsetX = GridBuilder.transform.position.x - (GridBuilder.Width / 2);
+			float offsetY = GridBuilder.transform.position.y + (GridBuilder.Height / 2);
+			
+			Point2D startPoint = new Point2D (
+				Mathf.Ceil (Mathf.Abs (offsetX - start.x) / GridBuilder.NodeSize),
+				Mathf.Ceil (Mathf.Abs(offsetY - start.y) / GridBuilder.NodeSize)
+			);
+
+			Point2D endPoint = new Point2D (
+				Mathf.Ceil (Mathf.Abs(offsetX - target.x) / GridBuilder.NodeSize),
+				Mathf.Ceil (Mathf.Abs(offsetY - target.y) / GridBuilder.NodeSize)
+			);
+
+			for (int y = 0; y < GridBuilder.Grid.GetLength(1); y ++) {
+				string row = "";
+				for (int x = 0; x < GridBuilder.Grid.GetLength(0); x++) {
+					if (GridBuilder.Grid[x, y].Passable) {
+						row += "-";
+					} else {
+						row += "x";
+					}
+				}
+
+				Debug.Log(row);
+			}
+
+			//Point2D[] path = AStar.Seek (GridBuilder.Grid, startPoint, endPoint);
+			Point2D[] path = null;
+			if (path == null) {
+				return new Vector2[0];
+			}
+
+			Vector2[] returnPath = new Vector2 [path.Length];
+			for (int i = 0; i < path.Length; i++) {
+				returnPath[i] = new Vector2(
+					(path[i].X * GridBuilder.NodeSize) + offsetX,
+					(path[i].Y * GridBuilder.NodeSize) + offsetY
+				);
+			}
+
+			return returnPath;
+		}
+
+		public Vector2[] SeekAsync(Vector2 target)
+		{
+			return SeekAsync (GetStartPosition(), target);
+		}
+
+		public Vector2[] SeekAsync(Vector2 start, Vector2 target)
+		{
+			return Seek (start, target);
+		}
+
+		public Vector2 GetStartPosition()
+		{
+			Vector2 position = transform.position;
+			Renderer renderer = gameObject.GetComponent<Renderer> ();
+
+			if (StartPathFrom == StartPositionType.Top && renderer != null ) {
+				position.y = renderer.bounds.max.y;
+			}
+
+			if (StartPathFrom == StartPositionType.Bottom) {
+				position.y = renderer.bounds.min.y;
+			}
+
+			return position;
 		}
 	}
 }
--- Assets/PurpleKingdomGames/Pathfinding/Components/GridBuilder2DComponent.cs
+++ Assets/PurpleKingdomGames/Pathfinding/Components/GridBuilder2DComponent.cs
@@ -1,25 +1,43 @@
 ﻿using UnityEngine;
+using PurpleKingdomGames.Core.Pathfinding;
+using PurpleKingdomGames.Unity.Core;
 
 namespace PurpleKingdomGames.Unity.Components
 {
     public class GridBuilder2DComponent : MonoBehaviour
     {
+		public LayerMask LayerMask = -1;
+		public float NodeSize = 0.5f;
+		public float Width = 10;
+		public float Height = 10;
+
+		[HideInInspector]
+		public GridNode2D[,] Grid;
+
         // Use this for initialization
         private void Start()
         {
+			Recalculate ();
         }
 
         // Update is called once per frame
-        private void Update()
+        public void Update()
         {
             // TODO: Show a grid in the editor window (only when game isn't running)
-            // TODO: Use grid location to build grid once, when starting
             // TODO: Allow the grid to be saved to a file
             // TODO: Allow the grid to be loaded from a file
-            // TODO: Create seeker component
-            // TODO: Allow the Seeker to seek Synchronously
-            // TODO: Allow the Seeker to seek Asynchronously
-            // TODO: Translate Point2D grid positions to world space
-        }
+			// TODO: Allow the Seeker to seek Asynchronously
+			Recalculate ();
+		}
+		
+		public void Recalculate()
+		{
+			Vector2 position = transform.position;
+			position.x -= Width / 2;
+			position.y -= Height / 2;
+
+			Rect bounds = new Rect (position, new Vector2 (Width, Height));
+			Grid = GridBuilder2D.BuildGrid (bounds, NodeSize, LayerMask);
+		}
     }
 }
